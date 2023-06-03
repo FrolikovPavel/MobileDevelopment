712ac73213d325fa20ab984d774f3e9ce0a77c63
FrolikovPavel <125368721+FrolikovPavel@users.noreply.github.com>
2023-06-03 22:51:21 +0300

    Lesson8

---
+++ Lesson8/yandexmaps/src/main/java/ru/mirea/frolikov/yandexmaps/App.java
@@ -0,0 +1,16 @@
+package ru.mirea.frolikov.yandexmaps;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "3d9fc319-c13d-45fc-b1ce-c6a69a5f3982";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
